ac72a1d548b3b57daf3a64b936f6fff540a828ba
Hector <baseballhector@gmail.com>
2023-06-15 21:32:50 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function component() {
+  const element = document.createElement("div");
+
+  element.innerHTML = "Test print display";
+
+  return element;
+}
+
+const content = document.getElementById("content");
+content.appendChild(component());
