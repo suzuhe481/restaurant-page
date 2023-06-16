3107d2b73efa126b2821683b4c1e11374884250a
Hector <baseballhector@gmail.com>
2023-06-16 18:58:07 -0400

    Manages tab switching

--- src/index.js
+++ src/index.js
@@ -1,43 +1,56 @@
-import "./style.css";
+import "./styles/style.css";
+import "./styles/home.css";
+import "./styles/contact.css";
 
-function homePage() {
-  // Creates header element.
-  const header = document.createElement("div");
-  header.id = "header";
+import navbar from "./components/navbar";
+import mainContent from "./components/mainContent";
 
-  //   Creates container element for menu items.
-  const menuItemsContainer = document.createElement("div");
-  menuItemsContainer.id = "menu-items";
+// Pages.
+import homeContent from "./pages/home";
+import menuContent from "./pages/menu";
+import contactContent from "./pages/contact";
 
-  // Creates menu items elements
-  const menuItemHome = document.createElement("div");
-  const menuItemMenu = document.createElement("div");
-  const menuItemContact = document.createElement("div");
-
-  menuItemHome.classList.add("menu-item");
-  menuItemMenu.classList.add("menu-item");
-  menuItemContact.classList.add("menu-item");
-
-  // Adds text to menu items.
-  menuItemHome.innerHTML = "Home";
-  menuItemMenu.innerHTML = "Menu";
-  menuItemContact.innerHTML = "Contact";
-
-  // Adds menu items to container
-  menuItemsContainer.appendChild(menuItemHome);
-  menuItemsContainer.appendChild(menuItemMenu);
-  menuItemsContainer.appendChild(menuItemContact);
-
-  // Adds menu container to header.
-  header.appendChild(menuItemsContainer);
-
-  return header;
-}
-
+// Element everything will be attached to.
 const content = document.getElementById("content");
-content.appendChild(homePage());
+
+// Adding navbar to page.
+content.appendChild(navbar());
 
 // Adds the main-content element to page.
-const mainContent = document.createElement("div");
-mainContent.id = "main-content";
-content.appendChild(mainContent);
+content.appendChild(mainContent());
+
+// Getting tabs.
+const tabs = document.getElementsByClassName("menu-item");
+const homeTab = tabs[0];
+const menuTab = tabs[1];
+const contactTab = tabs[2];
+
+// Adding event listener to each tab.
+homeTab.addEventListener("click", displayHome);
+menuTab.addEventListener("click", displayMenu);
+contactTab.addEventListener("click", displayContact);
+
+function displayHome() {
+  // Main content of current page.
+  const mainContentElement = document.getElementById("main-content");
+  mainContentElement.innerHTML = "";
+  mainContentElement.appendChild(homeContent());
+}
+
+function displayMenu() {
+  // Main content of current page.
+  const mainContentElement = document.getElementById("main-content");
+  mainContentElement.innerHTML = "";
+  mainContentElement.appendChild(menuContent());
+}
+
+function displayContact() {
+  // Main content of current page.
+  const mainContentElement = document.getElementById("main-content");
+  mainContentElement.innerHTML = "";
+  mainContentElement.appendChild(contactContent());
+}
+
+// Puts default home page content first.
+const mainContentElement = document.getElementById("main-content");
+mainContentElement.appendChild(homeContent());
